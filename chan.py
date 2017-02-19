c11a37d89c110dd1bb877e02e70449f0277ac40e
iceysteel <potatomuffin.zse@gmail.com>
2017-02-18 22:58:49 -0800

    committing all files, working on getting forms done for posting rn

---
+++ chan.py
@@ -0,0 +1,138 @@
+from flask import Flask
+#use markup later to escape html in strings that come from users
+from flask import Markup
+from flask import request
+import json
+from time import gmtime, strftime
+
+#REMOVE IN PRODUCTION FOR TESTING ONLY
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80)
+
+#remove the static url path varible when we go to production
+app = Flask(__name__, static_url_path='/static/')
+
+#---------------classes for main data structures
+class Post(object):
+	"""each post is its own little torrent, this contains all the data to dl it"""
+	def __init__(self, id, magnet):
+		self.post_id = id
+		self.post_magnet_uri = magnet
+		self.reputation = 0
+		#post time is a string
+		self.post_time = strftime("%Y-%m-%d+%H:%M:%S", gmtime())
+
+
+class Thread(Post):
+	"""a thread is a post that has an array of other posts related to that thread in it"""
+	def __init__(self, thr_id, magnet, title_param):
+		super(Thread, self).__init__(thr_id, magnet)
+		self.title = title_param
+		self.thread_id = thr_id
+		#the array of posts in this thread
+		self.posts = []
+
+	def toJSON(self):
+		return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+#---------------end classes-----------
+
+#list that will hold all the threads on the chan in memory
+global_threads = []
+
+#a hardcoded thread for testing
+testThread = Thread(0, 'magnet:?xt=urn:btih:01047ad40ce0ee28f729d6181083207c22f452ff&dn=post.txt&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com', 'This is the first thread!')
+
+#testPost = Post(1, 'magnet::this is another magnet link')
+#testPost2 = Post(2, 'magnet::this is another another magnet link')
+
+#testThread.posts.append(testPost)
+#testThread.posts.append(testPost2)
+
+
+global_threads.append(testThread)
+#testThread.title = "this not a test"
+#testThread.thread_id = 5
+#global_threads.append(testThread)
+#----------------end test data-------------
+
+#global post counter to be used for ids
+#CHANGE THIS BACK TO 0 AT SOME POINT
+post_count = 2
+
+def get_thread_by_id(thread_id_to_find):
+#please forgive me, this method is fucking awful, 
+#fix the algroithm and make it binary search asap
+	return next((thread for thread in global_threads if thread.post_id==thread_id_to_find), None)
+
+#-----------------application routes -------------------
+#since this is served statically it needs to be removed once nginix is setup
+@app.route('/')
+def index():
+	return app.send_static_file('client.html')
+
+
+#HUGE SECURITY PROBLEM, REMOVE WHEN GOING TO PRODUCTION
+@app.route('/static/<string:static_file>')
+def temp_static_files(static_file):
+	return app.send_static_file(static_file)
+
+
+@app.route("/catalog")
+def hello():
+    return json.dumps(global_threads, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+#returns contents of a thread
+@app.route("/thread/<int:thread_id>")
+def show_thread(thread_id):
+	if get_thread_by_id(thread_id) == None:
+		return 'there was a problem finding thread#' + str(thread_id)
+	else:
+		return get_thread_by_id(thread_id).toJSON()
+
+#this route takes POST requests and adds a post to a thread
+@app.route("/post/<int:thread_id>", methods=['GET','POST'])
+def create_post(thread_id):
+	#error = None
+	if request.method == 'POST':
+		#make sure we were sent the right data
+		if request.form['magnet']:
+			if get_thread_by_id(thread_id) == None:
+				return 'there was a problem finding thread#' + str(thread_id)
+			else:
+				#calculate the new id
+				global post_count
+				post_count = post_count + 1
+				#create a new post and append to the 
+				new_post = Post(post_count, request.form['magnet'])
+				#sometimes i really love these one liners you can make in python
+				get_thread_by_id(thread_id).posts.append(new_post)
+	else:
+		return 'wrong kinda request bro.'
+
+	return 'OK'
+
+#this route makes a new thread
+@app.route('/post/thread/', methods=['GET','POST'])
+def create_thread():
+	if request.method == 'POST':
+		if request.form['magnet'] and request.form['title']:
+			#calculate the new id
+			global post_count
+			post_count = post_count + 1
+			#create a new post and append to the 
+			new_thread = Thread(post_count, request.form['magnet'], request.form['title'])
+			global_threads.append(new_thread)
+	else:
+		return 'wrong kinda request bro.'
+
+	return 'OK'
+
+
+
+
+	
+
+
+		
+
+
---
+++ static/main.js
@@ -0,0 +1,127 @@
+//I really hate writing javascript
+
+//this initializes a webtorrent client
+var client = new WebTorrent();
+client.on('error', function (err) { console.log(err);})
+
+//make request to server get the catalog, 
+//right now this also has all the contents for every thread
+var request = new XMLHttpRequest();
+request.open('GET', '/catalog', true);
+
+request.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success! put parsed stuff in data
+    var data = JSON.parse(this.response);
+    consumeData(data);
+  } else {
+    // We reached our target server, but it returned an error
+    console.log('server fucked up fam')
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+  console.log('connection error')
+};
+
+request.send();
+//-----------------end request code---------
+
+//this function takes the parsed data and appends it to the html as post elements
+function consumeData(dataArray){
+	// the contents of data are in this format Arrayofthreads[thread[posts]]
+	console.log(dataArray);
+	//iterate through all the threads
+	for (var i = 0, len = dataArray.length; i < len; i++) {
+	  thread = dataArray[i];
+
+	  appendToBody(thread, true)
+
+	  //now loop through the posts in the thread
+	  for (var j = 0, len = thread.posts.length; j < len; j++) {
+	  	appendToBody(thread.posts[j], false);
+	  }
+
+	}
+
+}
+
+//this function was initially code just for threads that i modified
+//too lazy to change the comments
+function appendToBody(thread, isThread){
+	var threadDiv = document.createElement('div');
+	threadDiv.setAttribute('class', 'thread');
+
+	var introP = document.createElement('p');
+	introP.setAttribute('class', 'intro');
+	//in the furture replace anoymous with poster ids and stuff
+
+	var replyString =  'No.<a onclick=reply('+ thread.post_id +')>' + thread.post_id + '</a>';
+
+	if(isThread){
+
+		threadDiv.setAttribute('class', 'thread');
+		//find a way to do this the textcontent way, cause this is a security problem
+		introP.innerHTML = '<b>'+  thread.title  + '</b> ' + 'Anonymous' +  ' ' + thread.post_time + ' ' + replyString
+		//introP.textContent =  + 'Anonymous' +  ' ' + thread.post_time + ' ' + 'No.' + thread.post_id;
+		
+	}else{
+		threadDiv.setAttribute('class', 'post');
+		//introP.innerHTML = 'Anonymous' +  ' ' + thread.post_time + ' ' + 'No.' + thread.post_id;
+		introP.textContent = 'Anonymous' +  ' ' + thread.post_time + ' ' + replyString
+
+	}
+	//this div will contain files (pictures and stuff) in the future
+	var filesDiv = document.createElement('div');
+	filesDiv.setAttribute('class', 'files');
+	var imgTag = document.createElement('img');
+	imgTag.setAttribute('height', '200')
+	imgTag.setAttribute('onclick', 'resize(this)')
+
+
+	var postBody = document.createElement('div');
+	postBody.setAttribute('class', 'body');
+	//right now the post body is just gonna be the magnet link
+
+	//the post text or eventually html gets dropped into iframe elements for security
+	var postFrame = document.createElement('iframe');
+	postFrame.setAttribute('frameBorder', '0')
+
+	//postP.innerHTML = 'magnet link: ' + thread.post_magnet_uri;
+	var torrentId = thread.post_magnet_uri;  
+
+	//this is where we fetch the torrent
+	client.add(torrentId, function (torrent) {
+	  // first file is the post, second is the image for now
+	  var file = torrent.files[0];
+	  var image = torrent.files[1];
+
+	  //can only render text files to an iframe, may change this later
+	  file.renderTo(postFrame);
+	  image.renderTo(imgTag);
+	})
+
+	filesDiv.appendChild(imgTag);
+	postBody.appendChild(postFrame);
+
+
+	threadDiv.appendChild(introP);
+	threadDiv.appendChild(filesDiv);
+	threadDiv.appendChild(postBody);
+
+	//change this line later when we make a container
+	document.body.appendChild(threadDiv);
+}
+
+function resize(element){
+	if(element.hasAttribute('height')){
+		if(element.getAttribute('height') === '200'){
+			element.setAttribute('height', '')
+		}
+		else{
+			element.setAttribute('height', '200')
+		}
+	}
+	
+}
